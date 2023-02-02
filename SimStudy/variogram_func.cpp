a80d511f560977b10aab0e7219b79e705ec7b22f
sallen12 <sam.allen@stat.unibe.ch>
2023-02-02 12:05:38 +0100

    add weight function to variogram calculation

--- SimStudy/variogram_func.cpp
+++ SimStudy/variogram_func.cpp
@@ -6,14 +6,14 @@
 using namespace Rcpp;     
 
 // [[Rcpp::export]]
-double vsC(arma::colvec y, double p){
+double vario(arma::colvec y, arma::mat w_mat, double p){
   
   double out = 0;
   double d = y.size();
   for (int i = 1; i < (d+1); i++) {
     for (int j = i; j < (d+1); j++) {
-      double vy = pow(abs(y[i-1]-y[j-1]), p);
-      out += vy;
+      double vy = pow(abs(y[i-1] - y[j-1]), p);
+      out += 2*w_mat(i-1, j-1)*vy;
     }
   }
   return (out);
