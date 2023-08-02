396de0f6ce70d044cf8ea7100f5a89f7c8db7827
sallen12 <sam.allen@stat.unibe.ch>
2023-08-02 11:06:09 +0200

    add rcpp code to compute variogram

--- src/variogram_func.cpp
+++ src/variogram_func.cpp
@@ -3,17 +3,17 @@
 #include <cmath>
 #define _USE_MATH_DEFINES
 #include <math.h>
-using namespace Rcpp;     
+using namespace Rcpp;
 
 // [[Rcpp::export]]
 double vario(arma::colvec y, arma::mat w_mat, double p){
-  
+
   double out = 0;
   double d = y.size();
-  for (int i = 1; i < (d+1); i++) {
-    for (int j = i; j < (d+1); j++) {
-      double vy = pow(abs(y[i-1] - y[j-1]), p);
-      out += 2*w_mat(i-1, j-1)*vy;
+  for (int i = 0; i < d; i++) {
+    for (int j = 0; j < d; j++) {
+      double vy = pow(abs(y[i] - y[j]), p);
+      out += 2*w_mat(i, j)*vy;
     }
   }
   return (out);
